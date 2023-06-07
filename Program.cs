14c966ec6f341692148203b2d319eaf08322002c
Joniclei <jonicleibernardojunior@gmail.com>
2023-06-07 18:02:06 -0300

    as

--- Program.cs
+++ Program.cs
@@ -5,7 +5,23 @@ namespace Listando
   {
     public static void Main(string[]args)
     {
-      <Li
+      List <string> test = new List<string>();
+
+      test.Add("teste one");
+      test.Add("bgora que e 1");
+      test.Add("agora e o 2");
+      test.Add("dgora que e 1");
+      test.Add("bbora e o 2");
+
+      foreach(string x in test){
+        Console.WriteLine(x);
+      }
+      Console.WriteLine(test.Count);
+
+      int b = test.FindLastIndex(x => x[0] == 'b');
+
+      Console.WriteLine(b);
+
     }
   }
 }
