6b1f120a3a195675f155513c1d7c1b09bef14ec3
Joniclei <jonicleibernardojunior@gmail.com>
2023-06-07 18:17:36 -0300

    j

--- Program.cs
+++ Program.cs
@@ -3,17 +3,18 @@ namespace Listando
 {
   class Program
   {
-    public static void Main(string[]args)
+    public static void Main(string[] args)
     {
-      List <string> test = new List<string>();
+      List<string> test = new List<string>();
 
       test.Add("teste one");
       test.Add("bgora que e 1");
-      test.Add("agora e o 2");
-      test.Add("dgora que e 1");
-      test.Add("bbora e o 2");
+      test.Add("ago2");
+      test.Add("kkkdgora que e 1");
+      test.Add("bbo3");
 
-      foreach(string x in test){
+      foreach (string x in test)
+      {
         Console.WriteLine(x);
       }
       Console.WriteLine(test.Count);
@@ -22,6 +23,26 @@ namespace Listando
 
       Console.WriteLine(b);
 
+      List<string> test2 = test.FindAll(x => x.Length == 4);
+
+      Console.WriteLine("=============");
+
+      foreach (string p in test2)
+      {
+        Console.WriteLine(p);
+      }
+
+      test.Remove("bbo3");
+      test.Remove("teste one");
+      test.Remove("bgora que e 1");
+      test.Remove("ago2");
+      test.Add("dg ora que e 1");
+
+      foreach(string k in test){
+      Console.WriteLine(k);
+      }
+
     }
+
   }
 }
